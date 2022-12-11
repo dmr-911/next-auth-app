e7c75b81b57ecbb061756d210ebd15f8693e2477
dmr-911 <dewan.mizanur911@gmail.com>
2022-12-11 11:50:42 +0600

    mongo db project connected

---
+++ database/conn.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { MONGO_URL } from "../utils";
+
+const connectMongo = async () =>{
+    try {
+        const {connetion} = await mongoose.connect(MONGO_URL);
+
+        if(connetion.readyState == 1){
+            return Promise.resolve(true)
+        }
+    }catch (error){
+        return Promise.reject(error)
+    }
+}
+
+export default connectMongo;
